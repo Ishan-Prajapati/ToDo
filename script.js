a15a9d5a5e17ce1438a3d01423e0db2c82bce7ac
Ishan Prajapati <ishan.prajapati@aliansoftware.com>
2025-01-30 16:17:38 +0530

    update till delete item

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const itemform = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
+const clearbtn = document.getElementById('clear')
+const itemfilter = document.getElementById('filter')
+
 
 function additem(e){
     e.preventDefault();
@@ -18,6 +21,7 @@ function additem(e){
     button.appendChild(icon)
     li.appendChild(button)
     itemList.append(li)
+    checkUI()
     itemInput.value=='';
 }
 
@@ -33,4 +37,39 @@ function createIcon(classes){
     return icon;
 }
 
+function removeitem(e){
+   if(e.target.parentElement.classList.contains('remove-item')){
+    if(confirm("are you sure?")){
+
+        e.target.parentElement.parentElement.remove()
+        checkUI()
+    }
+    // console.log('click')
+   }
+  
+}
+
+function clearitem(e){
+    alert("are you want to delete all")
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild)
+    }
+}
+
+function checkUI(){
+    const items = document.querySelectorAll('li')
+    console.log(items)
+    if(items.length===0){
+        clearbtn.style.display='none'
+        itemfilter.style.display='none'
+    }
+    else{
+        clearbtn.style.display='block'
+        itemfilter.style.display='block'
+    }
+}
+
+clearbtn.addEventListener('click',clearitem)
+itemList.addEventListener('click',removeitem)
 itemform.addEventListener('submit',additem)
+checkUI()
